9a9c34e3a3ebc714612a93520fc97167d21499a0
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-11-08 16:27:41 +0100

    Add of Swagger2 configuration

--- backend/src/main/kotlin/com/zenika/lunchPlace/LunchPlaceApplication.kt
+++ backend/src/main/kotlin/com/zenika/lunchPlace/LunchPlaceApplication.kt
@@ -10,9 +10,16 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import java.util.*
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 open class LunchPlaceApplication {
 
     private val log = LoggerFactory.getLogger(LunchPlaceApplication::class.java)
@@ -20,6 +27,25 @@ open class LunchPlaceApplication {
     @Bean
     open fun objectMapperBuilder()
             = Jackson2ObjectMapperBuilder().modulesToInstall(KotlinModule())
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("LunchPlace API")
+                .description("API for LunchPlace applications")
+                .license("MIT License")
+                .licenseUrl("https://github.com/Zenika/lunchPlace/blob/master/LICENSE")
+                .version("1.0")
+                .build()
+    }
 
     @Bean
     open fun init(restaurantRepository: RestaurantRepository,
@@ -183,10 +209,10 @@ open class LunchPlaceApplication {
             userRepository.save(userManuD)
             userRepository.save(userChristophe)
 
-            val usedRestaurant0 :UsedRestaurant = UsedRestaurant(restaurantMadeInFrance, 1478559600000)
-            val usedRestaurant1 :UsedRestaurant = UsedRestaurant(restaurantSoGood, 1478473200000)
-            val usedRestaurant2 :UsedRestaurant = UsedRestaurant(restaurantTokyo, 1478214000000)
-            val usedRestaurant3 :UsedRestaurant = UsedRestaurant(restaurantLoveItaly, 1478127600000)
+            val usedRestaurant0: UsedRestaurant = UsedRestaurant(restaurantMadeInFrance, 1478559600000)
+            val usedRestaurant1: UsedRestaurant = UsedRestaurant(restaurantSoGood, 1478473200000)
+            val usedRestaurant2: UsedRestaurant = UsedRestaurant(restaurantTokyo, 1478214000000)
+            val usedRestaurant3: UsedRestaurant = UsedRestaurant(restaurantLoveItaly, 1478127600000)
             usedRestaurantRepository.save(usedRestaurant0)
             usedRestaurantRepository.save(usedRestaurant1)
             usedRestaurantRepository.save(usedRestaurant2)

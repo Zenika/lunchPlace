113416a0b3e1690ea1e53f46fceaf154146b9d5e
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2016-12-07 20:30:52 +0100

    fix(rancher): use the correct url

--- deployment/rancher/scripts/install-rancher-compose.sh
+++ deployment/rancher/scripts/install-rancher-compose.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 readonly RANCHER_COMPOSE_VERSION=0.12.0
-readonly RANCHER_COMPOSE_DOWNLOAD_URL=https://releases.rancher.com/compose/v${RANCHER_COMPOSE_VERSION}/rancher-compose-linux-amd64-v${RANCHER_COMPOSE_VERSION}.gz
+readonly RANCHER_COMPOSE_DOWNLOAD_URL=https://releases.rancher.com/compose/v${RANCHER_COMPOSE_VERSION}/rancher-compose-linux-amd64-v${RANCHER_COMPOSE_VERSION}.tar.gz
 readonly RANCHER_COMPOSE_TGZ=rancher-compose-linux-amd64-v${RANCHER_COMPOSE_VERSION}.tar.gz
 readonly RANCHER_COMPOSE_DIRECTORY=rancher-compose-v${RANCHER_COMPOSE_VERSION}
 

cae8df91ba39f116b942008b2ca556f2fdc78be0
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-02-13 12:37:36 +0100

    Add auth module

---
+++ frontend-web/src/auth.js
@@ -0,0 +1,70 @@
+import router from './Router';
+
+
+var options = {
+  language: 'fr',
+  allowSignUp: false,
+  rememberLastLogin: false,
+  auth: {
+    redirect: false
+  },
+  theme: {
+    primaryColor: '#0EB984'
+  }
+};
+
+var lock = new Auth0Lock(
+  process.env.Auth0_CLIENT_ID,
+  process.env.Auth0_CLIENT_DOMAIN,
+  options
+);
+
+lock.on("authenticated", function(authResult) {
+  lock.getUserInfo(authResult.accessToken, function(error, profile) {
+    if (error) { console.log(error); return; }
+    localStorage.setItem('accessToken', authResult.accessToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+    router.go('/home')
+  });
+});
+
+const auth = {
+
+  authenticated: !!localStorage.getItem('accessToken'),
+
+  lock : lock,
+
+  // To log out, we just need to remove the token
+  logout() {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('profile')
+    this.authenticated = false
+    router.go('/home')
+  },
+
+  checkAuth() {
+    var jwt = localStorage.getItem('accessToken')
+    if(jwt) {
+      this.authenticated = true
+    }
+    else {
+      this.authenticated = false
+    }
+    return !!jwt;
+  },
+
+  getToken() {
+    return localStorage.getItem('accessToken')
+  },
+
+  getProfile() {
+    return JSON.parse(localStorage.getItem('profile'))
+  },
+
+  // The object to be passed as a header for authenticated requests
+  getAuthHeader() {
+    return 'Bearer ' + localStorage.getItem('accessToken');
+  },
+}
+
+export default auth;
--- frontend-web/src/main.js
+++ frontend-web/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import App from './App';
 import Router from './Router';
+import auth from './auth';
+
+/* https://github.com/auth0-blog/vue-jwt-authentication */
 
 /* eslint-disable no-new */
+
+Vue.use(VueResource);
+
+Vue.http.interceptors.push((request, next)  => {
+  //Ajout du token
+  if(auth.checkAuth())
+    request.headers['Authorization'] = auth.getAuthHeader();
+
+  next((response) => {
+    if(response.status == 401 ) {
+      auth.logout();
+      Router.go('/');
+    }
+  });
+
+});
 
 new Vue({
   el: '#app',

8d6f4658c9bf944c8f499113ad5f6ce1eac663e6
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-02-13 17:49:11 +0100

    fix(ci): build the docker image
    - temp push

--- scripts/ci/frontend/test--override.sh
+++ scripts/ci/frontend/test--override.sh
@@ -5,6 +5,9 @@ set -x
 if [ -f build-frontend.txt ]; then
     cd frontend-web
     npm run build --env=dev
+    docker build --rm=false -t zenika/lunchplace_frontend:ci-000 .
+    docker login -e infos-lille@zenika.com -u $DOCKER_USER -p $DOCKER_PASS
+    docker push zenika/lunchplace_frontend:ci-000
 else
     echo "Noting to do"
 fi

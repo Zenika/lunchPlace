221b55162ece823713e471f9682b33da82c93cfb
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-02-13 19:04:15 +0100

    Setup ci for frontend (#30)
    * fix(ci/frontend): launch npm build
    * fix(ci): build the docker image
    * fix(npm): add dep to bootstrap
    * chore(docker/frontend): ignore the node_modules directories
    * chore(rancher): get ready to deploy frontend on rancher

--- scripts/ci/common/set-build-target.sh
+++ scripts/ci/common/set-build-target.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -x
 
-git diff-tree --no-commit-id --name-only ${CIRCLE_SHA1} | grep backend
+echo "Listing all diffs"
+git diff-tree --no-commit-id --name-only -r ${CIRCLE_SHA1}
+
+git diff-tree --no-commit-id --name-only -r ${CIRCLE_SHA1} | grep backend
 if [ $? == 0 ]; then
     touch build-backend.txt
     echo "Will build backend"
@@ -9,7 +12,7 @@ else
     echo "Will NOT build backend"
 fi
 
-git diff-tree --no-commit-id --name-only ${CIRCLE_SHA1} | grep frontend-web
+git diff-tree --no-commit-id --name-only -r ${CIRCLE_SHA1} | grep frontend
 if [ $? == 0 ]; then
     touch build-frontend.txt
     echo "Will build frontend"

1991675d5dc3d9982694a0e76cd3f48ac2f2cfa6
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-02-13 14:08:13 +0100

    chore(ci): build backend only if needed

--- scripts/ci/backend/dependencies--override.sh
+++ scripts/ci/backend/dependencies--override.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -x
 
 docker run --rm -v `pwd`:/project --workdir /project/backend -u `id -u`:`id -g` metahelicase/gradle:3.1 --no-daemon --console plain build
---
+++ scripts/ci/backend/deployment--master--commands.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo "Not Implemented Yet"

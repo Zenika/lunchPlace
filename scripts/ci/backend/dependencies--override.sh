12ff3eeb6dd31b96159620a76d06e9883457e18d
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-02-16 18:33:43 +0100

    chore(ci): put back every ci logic into the circle yaml config file (#33)
    * chore(ci): put back every ci logic into the circle yaml config file
    * chore(ci): deploy frontend on rancher!

--- scripts/ci/backend/dependencies--override.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-docker run --rm -v `pwd`:/project --workdir /project/backend -u `id -u`:`id -g` metahelicase/gradle:3.1 --no-daemon --console plain build
--- scripts/ci/frontend/dependencies--override.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-cd frontend-web
-npm install

224e7890116e24e0e095fff61eb5a160ee0d198d
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-03-17 17:05:36 +0100

    [fix] specify document

--- backend/src/main/kotlin/com/zenika/lunchPlace/restaurant/category/FoodCategory.kt
+++ backend/src/main/kotlin/com/zenika/lunchPlace/restaurant/category/FoodCategory.kt
@@ -1,12 +1,13 @@
 package com.zenika.lunchPlace.restaurant.category
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import java.io.Serializable
 
 /**
  * A FoodCategory is like a tag : "Asian", "Chinese", "French", "Fat", "Green", ...
  */
-
+@Document(collection = "foodCategory")
 data class FoodCategory(
         val name: String = "",
         @Id val id: String? = null
